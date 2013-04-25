9cd7cab6e07866811ba78397d9cfbe96b8f7a470
Jeff Risberg <jrisberg@aol.com>
2013-04-25 15:47:48 -0700

    improved configuration

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,18 +7,20 @@ CKEDITOR.editorConfig = function( config )
 {
   config.toolbar = 'MyToolbar';
  
-  config.toolbar_MyToolbar =
-  [
-    { name: 'document', items : [ 'NewPage','Preview' ] },
-    { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-    { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
-    { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
-                 ,'Iframe' ] },
-    '/',
-    { name: 'styles', items : [ 'Styles','Format' ] },
-    { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
-    { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
-    { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-    { name: 'tools', items : [ 'Maximize','-','About' ] }
+  config.toolbar_MyToolbar = [
+    ['Maximize'],   
+    ['pyroimages', 'pyrofiles'],
+    ['Cut','Copy','Paste','PasteFromWord'],
+    ['Undo','Redo','-','Find','Replace'],
+    ['Link','Unlink'],
+    ['Table','HorizontalRule','SpecialChar'],
+    ['Bold','Italic','StrikeThrough'],
+    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl'],
+    
+    ['Format', 'FontSize', 'Subscript','Superscript', 'NumberedList','BulletedList','Outdent','Indent','Blockquote'],
+    ['ShowBlocks', 'RemoveFormat', 'Source']
   ];
+  
+  config.width = 800;
+  config.height = 200;
 };
