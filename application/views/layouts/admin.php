83cee0a4ad0867063fb7a0a06f068ad1efab09f6
Jeff Risberg <jrisberg@aol.com>
2014-04-16 16:54:26 -0700

    Updated jQuery version

--- application/views/layouts/admin.php
+++ application/views/layouts/admin.php
@@ -14,7 +14,7 @@
       <?php $this->load->view('_footer');?>
     </div>
     
-    <script src="<?php echo base_url();?>assets/js/jquery-1.8.3.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery-1.10.2.min.js"></script>
     <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
   </body>
 </html>  
